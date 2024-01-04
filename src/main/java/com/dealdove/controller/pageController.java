2ec6e505fc31f8fa68ab6a0987a34bf5684911d8
TungYiShen <eric60106@gmail.com>
2024-01-04 11:20:29 +0800

    add static view

--- src/main/java/com/dealdove/controller/pageController.java
+++ src/main/java/com/dealdove/controller/pageController.java
@@ -1,10 +1,18 @@
 package com.dealdove.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class pageController {
-    public String index() {
-        return "index";
+
+    @GetMapping("/01_home_page")
+    public String test(Model model){
+        return "01_home_page";
+    }
+    @GetMapping("/04_product_page")
+    public String productPage(Model model){
+        return "04_product_page";
     }
 }
