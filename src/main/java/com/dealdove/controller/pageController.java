8ef60a1c4201fb5ba22aa19db4866604c2014407
TungYiShen <eric60106@gmail.com>
2024-01-04 11:54:24 +0800

    branch test

--- src/main/java/com/dealdove/controller/pageController.java
+++ src/main/java/com/dealdove/controller/pageController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class pageController {
     //你媽的這個是什麼鬼
+    //branch test 新增 不該出現在dev
+    //dev 不會在main上更新
 
     @GetMapping("/01_home_page")
     public String test(Model model){
