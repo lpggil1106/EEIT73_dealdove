e2ab40c79bd6c2da3bff3571836e5c399f27daaf
youjia <look53486@gmail.com>
2024-01-04 11:31:38 +0800

    test

--- src/main/java/com/dealdove/controller/pageController.java
+++ src/main/java/com/dealdove/controller/pageController.java
@@ -12,7 +12,7 @@ public class pageController {
         return "01_home_page";
     }
 
-//    test
+//    test123
     @GetMapping("/04_product_page")
     public String productPage(Model model){
         return "04_product_page";
