10bfb331d4df42788677d17013e3e57d1b320154
TungYiShen <eric60106@gmail.com>
2024-01-10 09:31:22 +0800

    application.properties 更新專案目標視圖目標檔名

--- src/main/java/com/dealdove/dealdove/controller/pageController.java
+++ src/main/java/com/dealdove/dealdove/controller/pageController.java
@@ -1,40 +1,13 @@
 package com.dealdove.dealdove.controller;
 
-import com.dealdove.dealdove.model.User;
-import com.dealdove.dealdove.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
+@Controller
 public class pageController {
 
-    @Autowired
-    private UserService userService;
-
-
-    @GetMapping("/01_home_page")
-    public String test(Model model){
-        return "01_home_page";
+    @GetMapping("/test")
+    public String test(){
+        return "test";
     }
-
-    //    test123456
-    @GetMapping("/04_product_page")
-    public String productPage(Model model){
-        return "04_product_page";
-    }
-
-    @RequestMapping("/link")
-    public List<User> test2(){
-        //回傳userService查詢到的資料
-        System.out.println(userService.findAllUsers()+"123456789");
-        System.out.println("123456789");
-        return userService.findAllUsers();
-    }
-
 }
---
+++ src/main/java/com/dealdove/dealdove/controller/userController.java
@@ -0,0 +1,39 @@
+package com.dealdove.dealdove.controller;
+
+import com.dealdove.dealdove.model.User;
+import com.dealdove.dealdove.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class userController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/01_home_page")
+    public String test(Model model){
+        return "01_home_page";
+    }
+
+    //    test123456
+    @GetMapping("/04_product_page")
+    public String productPage(Model model){
+        return "04_product_page";
+    }
+
+    @RequestMapping("/link")
+    public List<User> test2(){
+        //回傳userService查詢到的資料
+        System.out.println(userService.findAllUsers()+"123456789");
+        System.out.println("123456789");
+        return userService.findAllUsers();
+    }
+
+}
