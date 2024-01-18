01526e519c121a8124eaa3532965860e9d2e8783
Yuri <yuri204904@gmail.com>
2024-01-18 16:23:17 +0800

    ProductModelAssociateTableRepository inherited fixed

--- src/main/java/com/dealdove/dealdove/interfaces/ProductModelAssociateTableRepository.java
+++ src/main/java/com/dealdove/dealdove/interfaces/ProductModelAssociateTableRepository.java
@@ -1,5 +1,7 @@
 package com.dealdove.dealdove.interfaces;
 
+import com.dealdove.dealdove.model.ProductCategory;
+import com.dealdove.dealdove.model.ProductModelAssociateTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductModelAssociateTableRepository extends JpaRepository<ProductModelAssociateTableRepository, Long> {
-
+public interface ProductModelAssociateTableRepository extends JpaRepository<ProductModelAssociateTable, Long> {
+    @Query(value = "SLELCT * FROM productmodelassociateTable" , nativeQuery = true)
+    List<ProductCategory> findAllProductCategories();
 }
