f76c66a3725b7a3f7df7fe27b3adf60ec8651d77
Yuri <yuri204904@gmail.com>
2024-01-27 23:50:24 +0800

    product price quantity

--- src/main/java/com/dealdove/dealdove/controller/productPageController.java
+++ src/main/java/com/dealdove/dealdove/controller/productPageController.java
@@ -1,6 +1,7 @@
 package com.dealdove.dealdove.controller;
 
 
+import com.dealdove.dealdove.model.ModelInfo;
 import com.dealdove.dealdove.model.Product;
 import com.dealdove.dealdove.service.ProductPageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,15 @@ public class productPageController {
         if (!specifications.isEmpty()) {
             return new ResponseEntity<>(specifications, HttpStatus.OK);
         } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("/{productid}/{firstModelid}/{secondModelid}/modelinfo")
+    public ResponseEntity<ModelInfo> findModelInfoByProductIDAndFirstModelIDAndSecondModelID(@PathVariable Integer productid, @PathVariable Integer firstModelid, @PathVariable Integer secondModelid){
+        ModelInfo modelinfo = productPageService.findModelInfoByProductIDAndFirstModelIDAndSecondModelID(productid, firstModelid, secondModelid);
+        if(modelinfo != null){
+            return new  ResponseEntity<>(modelinfo, HttpStatus.OK);
+        }else{
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
